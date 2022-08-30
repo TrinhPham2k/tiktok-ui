ee73c6cd161965f6fda62d88223490cc7b7ba289
Trinh Pham <trinh.pham@vinasaver.vn>
2022-08-31 01:05:06 +0700

    Bai 62:Cấu hình Router/Layout cho dự án

---
+++ tiktok/src/routes/index.js
@@ -0,0 +1,17 @@
+import { HeaderOnly } from '~/components/Layout';
+import Home from '~/pages/Following';
+import Following from '~/pages/Home';
+import Profile from '~/pages/Profile';
+import Search from '~/pages/Search';
+import Upload from '~/pages/Upload';
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/profile', component: Profile },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '/search', component: Search, layout: null },
+];
+
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
