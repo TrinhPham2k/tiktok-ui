536af6ca328dfde05feef1ae331848406e8c6496
Trinh Pham <trinh.pham@vinasaver.vn>
2022-08-31 13:16:50 +0700

    Bai 64:Xây dựng UI phần Header(logo and search)

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,12 +1,31 @@
 import styles from './Header.module.scss';
 import classNames from 'classnames/bind';
 
-const cx = classNames.bind(styles);
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
 
+import images from '~/assets/images';
+const cx = classNames.bind(styles);
 function Header() {
     return (
         <header className={cx('wrapper')}>
-            <div className={cx('inner')}></div>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt=" " />
+                </div>
+                <div className={cx('search')}>
+                    <input type="text" placeholder="Search acounts and videos" spellCheck={false} />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('action')}></div>
+            </div>
         </header>
     );
 }
